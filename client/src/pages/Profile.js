8846b4a43569ca3f42be2375e42e5dbaf58fa981
Jordan Ophoff <noreply>
2022-02-26 15:45:00 -0500

    client-side login/signup complete

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import Auth from '../utils/auth';
 
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendsList';
+import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 const Profile = () => {
-  const { username: userParams } = useParams();
+  const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: userParams },
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
   });
-  console.log(data);
-  const user = data?.user || {};
+  // console.log(data);
+  const user = data?.me || data?.user || {};
+
+  // redirect to personal profile page if username is the logged-in user's
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/profile" />;
+  }
 
   if (loading) {
     return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
+      </h4>
+    );
   }
 
   return (
@@ -31,16 +46,18 @@ const Profile = () => {
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
-          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+          <ThoughtList
+            thoughts={user.thoughts}
+            title={`${user.username}'s thoughts...`}
+          />
         </div>
 
-        <div className='col-12 col-lg-3 mb-3'>
+        <div className="col-12 col-lg-3 mb-3">
           <FriendList
-          username={user.username}
-          friendCount={user.friendCount}
-          friends={user.friends}
-          >
-          </FriendList>
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          ></FriendList>
         </div>
       </div>
     </div>
